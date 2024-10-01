38f57b64879781c5e49e4f29aee1cc8f7b851e3a
Balázs Tibor Morvay <balazsmorvay@yahoo.com>
2024-10-01 13:57:10 +0200

    Tries

--- Utils/SVM_utils.py
+++ Utils/SVM_utils.py
@@ -1,5 +1,10 @@
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import (
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+    precision_recall_fscore_support,
+)
+import numpy as np
 
 
 def train_svm(X, y, kernel, C):
@@ -17,11 +22,12 @@ def test_svm(model, x, y, L=None, b=None):
         test_predictions = model.predict(x)
     cm = confusion_matrix(y, test_predictions, labels=model.classes_)
     display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-    display
-    tn, fp, fn, tp = cm.ravel()
+
+    prec, rec, fbeta, supp = precision_recall_fscore_support(y, test_predictions)
     metrics = {
-        "accuracy": ((tp + tn) / (tp + tn + fp + fn + 1e-6)),
-        "recall": (tp / (tp + fn + 1e-6)),
-        "precision": (tp / (tp + fp + 1e-6)),
+        "accuracy": np.count_nonzero(test_predictions == y) / len(test_predictions),
+        "recall": rec,
+        "precision": prec,
+        "fscore": fbeta,
     }
     return metrics, display
--- Utils/joint_OT_mapping_linear_classreg.py
+++ Utils/joint_OT_mapping_linear_classreg.py
@@ -12,6 +12,7 @@ from ot.lp import emd
 import numpy as np
 import mlflow
 from ot.optim import cg
+from ot import mapping
 from ot.utils import dist, unif, list_to_array, kernel, dots
 import matplotlib.pyplot as plt
 
@@ -173,7 +174,7 @@ def joint_OT_mapping_linear(
     yt,
     mu=1,
     eta=0.001,
-    bias=False,
+    bias=True,
     verbose=False,
     verbose2=False,
     numItermax=100,
@@ -412,6 +413,275 @@ def joint_OT_mapping_linear(
         return G, L
 
 
+def joint_OT_mapping_kernel(
+    xs,
+    xt,
+    ys,
+    yt,
+    mu=1,
+    eta=0.001,
+    kerneltype="gaussian",
+    sigma=1,
+    bias=True,
+    verbose=False,
+    verbose2=False,
+    numItermax=100,
+    numInnerItermax=10,
+    stopInnerThr=1e-6,
+    stopThr=1e-5,
+    log=False,
+    class_reg=True,
+    **kwargs,
+):
+    r"""Joint OT and nonlinear mapping estimation with kernels as proposed in
+    :ref:`[8] <references-joint-OT-mapping-kernel>`.
+
+    The function solves the following optimization problem:
+
+    .. math::
+        \min_{\gamma, L\in\mathcal{H}}\quad \|L(\mathbf{X_s}) -
+        n_s\gamma \mathbf{X_t}\|^2_F + \mu \langle \gamma, \mathbf{M} \rangle_F +
+        \eta \|L\|^2_\mathcal{H}
+
+        s.t. \ \gamma \mathbf{1} = \mathbf{a}
+
+             \gamma^T \mathbf{1} = \mathbf{b}
+
+             \gamma \geq 0
+
+
+    where :
+
+    - :math:`\mathbf{M}` is the (`ns`, `nt`) squared euclidean cost matrix between samples in
+      :math:`\mathbf{X_s}` and :math:`\mathbf{X_t}` (scaled by :math:`n_s`)
+    - :math:`L` is a :math:`n_s \times d` linear operator on a kernel matrix that
+      approximates the barycentric mapping
+    - :math:`\mathbf{a}` and :math:`\mathbf{b}` are uniform source and target weights
+
+    The problem consist in solving jointly an optimal transport matrix
+    :math:`\gamma` and the nonlinear mapping that fits the barycentric mapping
+    :math:`n_s\gamma \mathbf{X_t}`.
+
+    One can also estimate a mapping with constant bias (see supplementary
+    material of :ref:`[8] <references-joint-OT-mapping-kernel>`) using the bias optional argument.
+
+    The algorithm used for solving the problem is the block coordinate
+    descent that alternates between updates of :math:`\mathbf{G}` (using conditional gradient)
+    and the update of :math:`\mathbf{L}` using a classical kernel least square solver.
+
+
+    Parameters
+    ----------
+    xs : array-like (ns,d)
+        samples in the source domain
+    xt : array-like (nt,d)
+        samples in the target domain
+    mu : float,optional
+        Weight for the linear OT loss (>0)
+    eta : float, optional
+        Regularization term  for the linear mapping L (>0)
+    kerneltype : str,optional
+        kernel used by calling function :py:func:`ot.utils.kernel` (gaussian by default)
+    sigma : float, optional
+        Gaussian kernel bandwidth.
+    bias : bool,optional
+        Estimate linear mapping with constant bias
+    verbose : bool, optional
+        Print information along iterations
+    verbose2 : bool, optional
+        Print information along iterations
+    numItermax : int, optional
+        Max number of BCD iterations
+    numInnerItermax : int, optional
+        Max number of iterations (inner CG solver)
+    stopInnerThr : float, optional
+        Stop threshold on error (inner CG solver) (>0)
+    stopThr : float, optional
+        Stop threshold on relative loss decrease (>0)
+    log : bool, optional
+        record log if True
+
+
+    Returns
+    -------
+    gamma : (ns, nt) array-like
+        Optimal transportation matrix for the given parameters
+    L : (ns, d) array-like
+        Nonlinear mapping matrix ((:math:`n_s+1`, `d`) if bias)
+    log : dict
+        log dictionary return only if log==True in parameters
+
+
+    .. _references-joint-OT-mapping-kernel:
+    References
+    ----------
+    .. [8] M. Perrot, N. Courty, R. Flamary, A. Habrard,
+       "Mapping estimation for discrete optimal transport",
+       Neural Information Processing Systems (NIPS), 2016.
+
+    See Also
+    --------
+    ot.lp.emd : Unregularized OT
+    ot.optim.cg : General regularized OT
+
+    """
+    xs, xt = list_to_array(xs, xt)
+    nx = get_backend(xs, xt)
+
+    ns, nt = xs.shape[0], xt.shape[0]
+
+    K = kernel(xs, xs, method=kerneltype, sigma=sigma)
+    if bias:
+        K1 = nx.concatenate((K, nx.ones((ns, 1), type_as=xs)), axis=1)
+        Id = nx.eye(ns + 1, type_as=xs)
+        Id[-1] = 0
+        Kp = nx.eye(ns + 1, type_as=xs)
+        Kp[:ns, :ns] = K
+
+        # ls regu
+        # K0 = K1.T.dot(K1)+eta*I
+        # Kreg=I
+
+        # RKHS regul
+        K0 = nx.dot(K1.T, K1) + eta * Kp
+        Kreg = Kp
+
+    else:
+        K1 = K
+        Id = nx.eye(ns, type_as=xs)
+
+        # ls regul
+        # K0 = K1.T.dot(K1)+eta*I
+        # Kreg=I
+
+        # proper kernel ridge
+        K0 = K + eta * Id
+        Kreg = K
+
+    if log:
+        log = {"err": []}
+
+    a = unif(ns, type_as=xs)
+    b = unif(nt, type_as=xt)
+    M = dist(xs, xt) * ns
+    M_ = M.clone().detach()
+    # Ötlet 2: legyenek a különböző classú minták nagyon távol egymástól
+    # Ez elvileg nem rontja el a konvexitást, mert nem függ sem gammától, sem L-től.
+    if class_reg:
+        for c in [2, 4]:
+            idx_s = np.where((ys != c) & (ys != -1))[0]
+            idx_t = np.where(yt == c)[0]
+
+            for j in idx_t:
+                M_[idx_s, j] = (
+                    M.max() * 1.0001
+                )  # Needed for numerical reasons (see: https://github.com/PythonOT/POT/issues/229#issuecomment-824616912)
+    M = M_
+
+    G = emd(a, b, M)
+
+    vloss = []
+
+    def loss(L, G):
+        """Compute full loss"""
+        return (
+            nx.sum((nx.dot(K1, L) - ns * nx.dot(G, xt)) ** 2)
+            + mu * nx.sum(G * M)
+            + eta * nx.trace(dots(L.T, Kreg, L))
+        )
+
+    def solve_L_nobias(G):
+        """solve L problem with fixed G (least square)"""
+        xst = ns * nx.dot(G, xt)
+        return nx.solve(K0, xst)
+
+    def solve_L_bias(G):
+        """solve L problem with fixed G (least square)"""
+        xst = ns * nx.dot(G, xt)
+        return nx.solve(K0, nx.dot(K1.T, xst))
+
+    def solve_G(L, G0):
+        """Update G with CG algorithm"""
+        xsi = nx.dot(K1, L)
+
+        def f(G):
+            return nx.sum((xsi - ns * nx.dot(G, xt)) ** 2)
+
+        def df(G):
+            return -2 * ns * nx.dot(xsi - ns * nx.dot(G, xt), xt.T)
+
+        G = cg(
+            a,
+            b,
+            M,
+            1.0 / mu,
+            f,
+            df,
+            G0=G0,
+            numItermax=numInnerItermax,
+            stopThr=stopInnerThr,
+        )
+        return G
+
+    if bias:
+        solve_L = solve_L_bias
+    else:
+        solve_L = solve_L_nobias
+
+    L = solve_L(G)
+
+    vloss.append(loss(L, G))
+
+    if verbose:
+        print(
+            "{:5s}|{:12s}|{:8s}".format("It.", "Loss", "Delta loss") + "\n" + "-" * 32
+        )
+        print("{:5d}|{:8e}|{:8e}".format(0, vloss[-1], 0))
+
+    # init loop
+    if numItermax > 0:
+        loop = 1
+    else:
+        loop = 0
+    it = 0
+
+    while loop:
+
+        it += 1
+
+        # update G
+        G = solve_G(L, G)
+
+        # update L
+        L = solve_L(G)
+
+        vloss.append(loss(L, G))
+
+        if it >= numItermax:
+            loop = 0
+
+        if abs(vloss[-1] - vloss[-2]) / abs(vloss[-2]) < stopThr:
+            loop = 0
+
+        if verbose:
+            if it % 20 == 0:
+                print(
+                    "{:5s}|{:12s}|{:8s}".format("It.", "Loss", "Delta loss")
+                    + "\n"
+                    + "-" * 32
+                )
+            print(
+                "{:5d}|{:8e}|{:8e}".format(
+                    it, vloss[-1], (vloss[-1] - vloss[-2]) / abs(vloss[-2])
+                )
+            )
+    if log:
+        log["loss"] = vloss
+        return G, L, log
+    else:
+        return G, L
+
+
 def compute_joint_OT_mapping(
     xs,
     xt,
@@ -518,22 +788,41 @@ def compute_joint_OT_mapping(
     License: MIT License
 
     """
-    G, L, loss = joint_OT_mapping_linear(
-        xs=xs,
-        xt=xt,
-        ys=ys,
-        yt=ys,
-        mu=mu,
-        eta=eta,
-        bias=bias,
-        verbose=verbose,
-        numItermax=numItermax,
-        numInnerItermax=numInnerItermax,
-        stopInnerThr=stopInnerThr,
-        stopThr=stopThr,
-        log=True,
-        class_reg=class_reg,
-    )
+
+    if method == "linear":
+        G, L, loss = joint_OT_mapping_linear(
+            xs=xs,
+            xt=xt,
+            ys=ys,
+            yt=ys,
+            mu=mu,
+            eta=eta,
+            bias=bias,
+            verbose=verbose,
+            numItermax=numItermax,
+            numInnerItermax=numInnerItermax,
+            stopInnerThr=stopInnerThr,
+            stopThr=stopThr,
+            log=True,
+            class_reg=class_reg,
+        )
+    elif method == "kernel":
+        G, L, loss = joint_OT_mapping_kernel(
+            xs=xs,
+            xt=xt,
+            ys=ys,
+            yt=ys,
+            mu=mu,
+            eta=eta,
+            bias=bias,
+            verbose=verbose,
+            numItermax=numItermax,
+            numInnerItermax=numInnerItermax,
+            stopInnerThr=stopInnerThr,
+            stopThr=stopThr,
+            log=True,
+            class_reg=class_reg,
+        )
 
     if log:
         for i, entry in enumerate(loss["loss"]):
